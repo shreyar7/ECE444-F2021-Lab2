7543e1fb17da278433f23b57924c547ba81d9e19
Shreya <shreya.rajendran7@gmail.com>
2021-09-25 15:52:02 -0400

    Modified hello.py to include Example 2-2.

--- hello.py
+++ hello.py
@@ -4,10 +4,16 @@ from flask import Flask
 #Declare app instance
 app = Flask(__name__)
 
-#Define Home Page
+#Define Home Page with Hello World
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+#Define user page with Hello Name
+#Using dynamic route
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
 
 if __name__ == '__main__':
     app.run(debug=True)
