ff451de9c313294260ef7e2cc7fea759e0c8ce41
Shreya <shreya.rajendran7@gmail.com>
2021-09-25 19:07:26 -0400

    Commit for Activity 3. Added html files for index page and user page with timestamp.

--- hello.py
+++ hello.py
@@ -1,19 +1,24 @@
-#Import flask module
-from flask import Flask
+#Import required functions
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 #Declare app instance
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 #Define Home Page with Hello World
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html', current_time=datetime.utcnow())
 
 #Define user page with Hello Name
 #Using dynamic route
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
 
 if __name__ == '__main__':
     app.run(debug=True)
