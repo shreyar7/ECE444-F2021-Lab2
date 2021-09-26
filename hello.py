c69a760c2924d39b0b7564b9e42a185dbee14ce4
Shreya <shreya.rajendran7@gmail.com>
2021-09-25 23:26:51 -0400

    Commit for Activity 4 Step 2. Added email field and flash messages.

--- hello.py
+++ hello.py
@@ -4,11 +4,13 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email
 from datetime import datetime
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[Email()])
     submit = SubmitField('Submit')
 
 #Declare app instance
@@ -22,12 +24,20 @@ moment = Moment(app)
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        
         old_name = session.get('name')
+        old_email = session.get('email')
+        
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+
         session['name'] = form.name.data
+        session['email'] = form.email.data
+            
         return redirect(url_for('index'))
-    return render_template('index.html', form = form, name = session.get('name'))
+    return render_template('index.html', form = form, name = session.get('name'), email = session.get('email'))
 
 #Define user page with Hello Name
 #Using dynamic route
